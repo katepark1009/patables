2b4fe9b61e92efc8bf496013f324f6c543a02fd3
Kate Park <kpark@hotbsoftware.com>
2019-12-11 14:20:14 -0800

    Storybook | Add proptype to rollup config global and external, cleanup package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
 import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
+import filesize from 'rollup-plugin-filesize'
 
 const config = {
   input: 'src/index.js',
-  external: ['react'],
+  external: ['react', 'prop-types'],
   output: {
     format: 'umd',
     name: 'countdown',
     globals: {
-      react: 'React'
+      react: 'React',
+      'prop-types': 'PropTypes'
     }
   },
   plugins: [
-    babel({ exclude: 'node_modules/**' }),
-    uglify()
+    babel({ exclude: ['node_modules/**', 'storybook', 'storybook-static', '.storybook', 'test'] }),
+    uglify(),
+    filesize()
   ]
 }
 
